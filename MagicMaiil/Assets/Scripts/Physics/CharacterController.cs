285021b224c755e4c00b9435e1acc9acd0d1f3e5
Zaire Wilson <Zaire.R.Wilson@gmail.com>
2020-06-15 18:52:54 -0400

    Basic movement, collision, and interactables

---
+++ MagicMaiil/Assets/Scripts/Physics/CharacterController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    private Rigidbody2D rb; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();     
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Move(Vector3 veloc)
+    {
+        rb.velocity = new Vector3(veloc.x + rb.velocity.x, veloc.y + rb.velocity.y, 0) * Time.deltaTime;
+
+    }
+
+}
---
+++ MagicMaiil/Assets/Scripts/Physics/Player.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    [SerializeField]
+    private float move_speed;
+    [SerializeField]
+    private float diag_speed; 
+    private Vector3 input;
+
+
+    public GameObject interaction_bubble; 
+    
+
+    private CharacterController char_cont; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        char_cont = this.GetComponent<CharacterController>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        input = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0);
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (input.x != 0 && input.y != 0)
+        {
+            char_cont.Move(input * move_speed * (diag_speed / 100));
+        }
+        else
+        {
+            char_cont.Move(input * move_speed);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Character" || collision.tag == "PickUp")
+        {
+            interaction_bubble.SetActive(true);
+        }
+
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.tag == "Character" || collision.tag == "PickUp")
+        {
+            interaction_bubble.SetActive(false);
+        }
+    }
+}
