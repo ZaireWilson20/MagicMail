df53f406d221c60bc122684b8bd93423f38fc66c
Leonardo Price <leonardo.price@gmail.com>
2020-06-15 21:01:14 -0400

    player animation

---
+++ MagicMaiil/Assets/playerVelocityReturn.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerVelocityReturn : MonoBehaviour
+{
+    Animator animator;
+    private float xMove = 0;
+    private float yMove = 0;
+    private Rigidbody2D rb;
+    private SpriteRenderer sr;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        xMove = rb.velocity.x;
+        yMove = rb.velocity.y;
+        Debug.Log(yMove);
+        animator.SetFloat("XMovement", xMove);
+        animator.SetFloat("YMovement", yMove);
+        if(  xMove >= 0)
+        {
+            sr.flipX = true;
+        }
+        else
+        {
+            sr.flipX = false;
+        }
+
+
+    }
+}
